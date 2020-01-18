6efc459d12ba0177999a64d2cbe2f4bea7afe016
Sebastian Jegerås <jegeras@gmail.com>
2020-01-18 16:13:03 +0100

    Add small testcase of parse_line
    Fix spelling in example-gcode
    Split out parse_line from parse

--- src/simple_machine.rs
+++ src/simple_machine.rs
@@ -7,6 +7,9 @@ pub struct SimpleMachine {
     x: (i32, i32),
     y: (i32, i32),
     z: (i32, i32),
+    steps_per_unit_x: i32,
+    steps_per_unit_y: i32,
+    steps_per_unit_z: i32,
 }
 
 impl SimpleMachine {
@@ -18,13 +21,27 @@ impl SimpleMachine {
             x: (0, 0),
             y: (0, 0),
             z: (0, 0),
+            steps_per_unit_x: 100,
+            steps_per_unit_y: 100,
+            steps_per_unit_z: 100,
         }
     }
 
     pub fn process(&mut self) -> i32 {
         match self.program.get(&self.pc) {
-            Some(gcode) => {
-                println!("pc:{} gcode:{:?}", self.pc, gcode);
+            Some(entry) => {
+                match &entry[0].command {
+                    // Movement
+                    'G' => match &entry[0].subcommand.0 {
+                        0 => println!("Rapid movement, {:?}", entry),
+                        1 => println!("Interpolated movement: {:?}", entry),
+                        _ => println!("Unsupported: {:?}", entry),
+                    },
+                    'O' => {
+                        println!("Set name of section");
+                    }
+                    _ => println!("Unsupported: {:?}", entry),
+                }
                 self.pc += self.step;
                 0
             }
--- src/simple_machine/gcode.rs
+++ src/simple_machine/gcode.rs
@@ -1,14 +1,99 @@
+use itertools::Itertools;
 use regex::Regex;
 use std::collections::HashMap;
 
-#[derive(Debug)]
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_without_params() {
+        {
+            let input = "N11 X0";
+
+            let result = parse_line(0, input).unwrap();
+
+            assert_eq!(11, result.0);
+            assert_eq!(
+                vec![GCode {
+                    command: 'X',
+                    subcommand: (0, 0),
+                    parameters: "".to_string()
+                }],
+                result.1
+            );
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
 pub struct GCode {
-    command: char,
-    subcommand: usize,
+    pub command: char,
+    pub subcommand: (usize, usize),
     parameters: String,
 }
 type GCodeBlock = Vec<GCode>;
 pub type GCodeProgram = HashMap<i32, GCodeBlock>;
+
+pub fn parse_line(linenumber: i32, line: &str) -> Option<(i32, GCodeBlock)> {
+    let valid_commands =
+        Regex::new(r"^(\s*(N)([0-9]+)\s)?\s?([A-Z])([-+.0-9]+\b)(\s(.*))?").unwrap();
+    match valid_commands.captures(line) {
+        Some(x) => {
+            // An N-command was stated, override the default linenumber
+            let validated_linenumber = match x.get(3) {
+                Some(value) => match value.as_str().parse::<i32>() {
+                    Ok(v) => v,
+                    Err(_) => linenumber,
+                },
+                None => linenumber,
+            };
+
+            let gcode = GCode {
+                command: match x.get(4) {
+                    Some(value) => match value.as_str().chars().next() {
+                        Some(c) => c,
+                        None => ' ',
+                    },
+                    None => ' ',
+                },
+
+                subcommand: match x.get(5) {
+                    Some(value) => {
+                        let parts = value
+                            .as_str()
+                            .split(".")
+                            .into_iter()
+                            .map(|v| match v.parse::<usize>() {
+                                Ok(v) => v,
+                                Err(_) => 0,
+                            })
+                            .collect::<Vec<usize>>();
+                        (
+                            match parts.get(0) {
+                                Some(v) => *v,
+                                None => 0,
+                            },
+                            match parts.get(1) {
+                                Some(v) => *v,
+                                None => 0,
+                            },
+                        )
+                    }
+                    None => (0, 0),
+                },
+
+                parameters: match x.get(7) {
+                    Some(value) => value.as_str().to_string(),
+                    None => "".to_string(),
+                },
+            };
+
+            Some((validated_linenumber, vec![gcode]))
+        }
+        None => None,
+    }
+}
 
 pub fn parse(filepath: String) -> GCodeProgram {
     let contents =
@@ -17,10 +102,6 @@ pub fn parse(filepath: String) -> GCodeProgram {
     // Detect empty lines and full line comments
     let invalid_line = Regex::new(r"(^\s*;)|(^$)|(^%)").unwrap();
     let clean_line = Regex::new(r"^([^;]*)").unwrap();
-    let valid_commands =
-        Regex::new(r"^(\s*(N)([0-9]+)\s)?\s?([A-Z])([-:.+0-9]+\b)(\s(.*)\b)?").unwrap();
-    //let valid_parameters =
-    //    Regex::new(r"([A-Z][-:.+0-9]+\s?|P'.*'\s?|\*[0-9]+\s?|!.*#\s?)").unwrap();
 
     let mut program = GCodeProgram::new();
 
@@ -34,37 +115,9 @@ pub fn parse(filepath: String) -> GCodeProgram {
             None => "",
         };
 
-        match valid_commands.captures(cleaned_line) {
-            Some(x) => {
-                linenumber = match x.get(3) {
-                    Some(value) => match value.as_str().parse::<usize>() {
-                        Ok(v) => v,
-                        Err(_) => linenumber,
-                    },
-                    None => linenumber,
-                };
-
-                let gcode = GCode {
-                    command: match x.get(4) {
-                        Some(value) => value.as_str().chars().next().unwrap(),
-                        None => ' ',
-                    },
-
-                    subcommand: match x.get(5) {
-                        Some(value) => match value.as_str().parse::<usize>() {
-                            Ok(v) => v,
-                            Err(_) => 0,
-                        },
-                        None => 0,
-                    },
-
-                    parameters: match x.get(7) {
-                        Some(value) => value.as_str().to_string(),
-                        None => "".to_string(),
-                    },
-                };
-
-                program.insert(linenumber as i32, vec![gcode]);
+        match parse_line(linenumber as i32, cleaned_line) {
+            Some((line, gcode)) => {
+                program.insert(line, gcode);
                 linenumber += 1;
             }
             None => (),
